5d94e2ba6d15c52375eb791c13dadc1671a02fad
Zeliha BAŞTÜRK DEMIR <zlhbstrk@hotmail.com>
2023-10-08 01:04:00 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { api } from '../axios.js'
+
+api.get('http://api.harundemir.kim/WeatherForecast').then((response) => {
+  console.log(response)
+})
+
+api.get('https://localhost:7045/WeatherForecast').then((response) => {
+  console.log(response)
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
